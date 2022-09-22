9ca4379dc50f7ddf683c51b27fb151f6b5105ae0
amal448 <56380854+amal448@users.noreply.github.com>
2022-09-22 01:40:31 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function black(){
+    document.getElementById("brown").src="/images/cocacola2.png";
+    document.getElementById("shape1").style.backgroundColor="black";
+    document.getElementById("learnmore").style.backgroundColor="black";
+    document.getElementById("headbg").style.backgroundColor="black";
+}
+function silver(){
+    document.getElementById("brown").src="/images/cocacola3.png"
+    document.getElementById("shape1").style.backgroundColor="black";
+    document.getElementById("learnmore").style.backgroundColor="red";
+    document.getElementById("headbg").style.backgroundColor="red";
+}
+function red(){
+    document.getElementById("brown").src="/images/cocacola5.png"
+    document.getElementById("shape1").style.backgroundColor="black";
+    document.getElementById("learnmore").style.backgroundColor="black";
+    document.getElementById("headbg").style.backgroundColor="black";
+    
+
+}
